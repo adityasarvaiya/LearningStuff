ea698627f3fec8f8b01d06698b5b777c0412c6f1
adityasarvaiya <adityasarvaiya96@gmail.com>
2018-10-15 00:58:10 +0530

    ada

--- Sem 1/ADA/assign 6/lcs.c
+++ Sem 1/ADA/assign 6/lcs.c
@@ -14,8 +14,8 @@ int maxof(int a, int b)
 void printmatrix(int l1, int l2 ,int matrix[l1][l2]) 
 {
     int i,j;
-    for (i = 0; i <= l1; i++) {
-        for (j = 0; j <= l2; j++) {
+    for (i = 0; i < l1; i++) {
+        for (j = 0; j < l2; j++) {
             //cout << matrix[i][j] << "  ";
             printf("%d ", matrix[i][j]);
         }
@@ -23,6 +23,42 @@ void printmatrix(int l1, int l2 ,int matrix[l1][l2])
         printf("\n");
     }
 }
+
+void printseq(char seq[], int index) 
+{
+    int i;
+    for (i = index; i >= 0; i--) {
+        printf("%c ", seq[i]);
+    }
+    printf("\n");
+}
+
+void makesequence(int len1, int len2, int matrix[len1][len2], int i, int j, char seq[], int *index, char s1[], char s2[])
+{
+    if (i == 0 || j == 0) {
+        printmatrix(len1, len2, matrix);
+        printf("\n");
+        printseq(seq, (*index));
+    } else { 
+        int flag = 0;
+        if (matrix[i][j] == matrix[i-1][j]) {
+            flag = 1;
+            makesequence(len1, len2, matrix, i-1, j, seq, index, s1, s2);
+        }
+
+        if (matrix[i][j] == matrix[i][j-1]) {
+            flag = 1;
+            makesequence(len1, len2, matrix, i, j-1, seq, index, s1, s2);
+        }
+
+        if (flag == 0) {
+            (*index)++;
+            seq[(*index)] = s1[i-1];
+            makesequence(len1, len2, matrix, i-1, j-1, seq, index, s1, s2);
+        }
+    }
+}
+
 
 int main()
 {
@@ -46,12 +82,10 @@ int main()
         matrix[0][j] = 0;
     }
 
-    printmatrix(len1, len2, matrix);    
-
-    for (i = 1; i <= len1; i++) {
-        for (j = 1; j <= len2; j++) {
-
-            if (s1[i] ==  s2[j]) {
+    for (i = 1; i < len1; i++) {
+        for (j = 1; j < len2; j++) {
+            
+            if (s1[i-1] ==  s2[j-1]) {
                 matrix[i][j] = matrix[i-1][j-1] + 1;
             } else {
                 matrix[i][j] = maxof(matrix[i-1][j], matrix[i][j-1]);
@@ -59,7 +93,9 @@ int main()
         }
     }
 
-    printmatrix(len1, len2, matrix);
+    char seq[100];
+    int index = -1;
+    makesequence(len1, len2, matrix, len1-1, len2-1, seq, &index, s1, s2);
 
     return 0;
 }
