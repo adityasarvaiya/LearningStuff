03f35eff24b775a135c19b21f2acc984b75111ec
adityasarvaiya <adityasarvaiya96@gmail.com>
2018-10-27 10:24:36 +0530

    PPR

--- Sem 1/PPR/Assign9 BTree/btree.c
+++ Sem 1/PPR/Assign9 BTree/btree.c
@@ -10,6 +10,7 @@ struct node {
     int count;
     int key[MAXKEYS];
     struct node * (ptr[MAXPTRS]);
+    struct node *par;
 };
 
 struct node * createNode(int data)
@@ -17,6 +18,7 @@ struct node * createNode(int data)
     struct node *curr = (struct node *) malloc(sizeof(struct node));
     curr->count = 1;
     curr->key[0] = data;
+    curr->par = NULL;
     int i;
     for (i = 0; i < (MINDIG*2); i++) {
         curr->ptr[i] = NULL;
@@ -40,7 +42,7 @@ void printinorder(struct node *root)
 
 void insertInNode(struct node **root, int data) 
 {
-    int i = 0,j, n = (*root)->count;
+    int i = 0, j, n = (*root)->count;
     printf("n : %d\n", n);
 
     for (i = n-1; i >= 0; i--) {
@@ -57,14 +59,71 @@ void insertInNode(struct node **root, int data)
     (*root)->count = (*root)->count + 1;
 }
 
+int newSearchIndex(struct node *root, int data)
+{
+    int i = 0, n = root->count;
+    for (i = 0; i < n; i++) {
+        if (root->key[i] > data) {
+            return i;
+        }
+    } 
+
+    return (n + 1);
+}
+
+
 void insert(struct node **root, int data) 
 {
     if ((*root) == NULL) {
         struct node *curr = createNode(data);
         *root = curr;
-    } else if ((*root)->count < (MINDIG*2 - 1)) {
-        insertInNode(root, data);
+    } else {
+        struct node *par = NULL;
+
+        while ((*root)->ptr[0] != NULL) {
+            par = (*root);
+            (*root) = (*root)->ptr[newSearchIndex(*root, data)];
+        }    
+
+        if ((*root)->count < (MINDIG*2 - 1)) {
+            insertInNode(root, data);
+        } else {
+            insertInNode(root, data);
+            struct node *newNode = (struct node *) malloc(sizeof(struct node));
+            int i, tmp = -1;
+            int mid = MINDIG;
+            //shift all pointers to new node
+            for (i = mid; i < MAXPTRS; i++) {
+                ++tmp;
+                newNode->ptr[tmp] = (*root)->ptr[i];
+                (*root)->ptr[i] = NULL;
+            }
+            
+            tmp = -1;
+            //shift keys to new node
+            for (i = mid+1; i < MAXKEYS; i++) {
+                ++tmp;
+                newNode->key[tmp] = (*root)->key[i];
+                (*root)->key[i] = -1;
+            }
+
+            (*root)->count = MINDIG;
+            newNode->count = tmp + 1;
+
+            // insert into a parent node
+            insert(&par, (*root)->key[MINDIG]);
+            if (par == NULL) {
+                struct node *curr = createNode((*root)->key[MINDIG]);
+                curr->ptr[0] = (*root);
+                curr->ptr[1] = newNode;
+                (*root) = curr;
+            } else if () {
+
+            }
+        }
+
     }
+
 }
 
 int main(int argc, char const *argv[])
