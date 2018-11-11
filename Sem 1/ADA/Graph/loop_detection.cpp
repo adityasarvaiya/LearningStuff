a83ef0c277313e882674be4b4b433c0ed4b1c6a2
adityasarvaiya <adityasarvaiya96@gmail.com>
2018-11-11 18:36:52 +0530

    added ust sir assignment

--- Sem 1/ADA/Graph/loop_detection.cpp
+++ Sem 1/ADA/Graph/loop_detection.cpp
@@ -5,7 +5,7 @@ int dfs(int *graph, int row, int V, int visited[], int par)
     int col, j, flag;
     visited[row] = 1;
 
-    for (col = 0; col < V; col++) {
+    for (col = row; col < V; col++) {
 
         if (graph[row * V + col] == 1) {
 
@@ -23,6 +23,46 @@ int dfs(int *graph, int row, int V, int visited[], int par)
     return 0;
 }
 
+int haveCycle(int *graph, int V) 
+{
+    int visited[V];
+    int row, i, flag = 0;
+    int index = -1;
+    int par = -1;
+
+    for (i = 0; i < V; i++) {
+        visited[i] = 0;
+    }
+
+    for (row = 0; row < V; row++) {   
+        
+        if (visited[row] == 0) {
+            flag = dfs(graph, row, V, visited, par);
+             
+            if (flag == 1) {
+                break;
+            }
+
+        }
+    }
+
+    return flag;
+}
+
+int findZeroDeg(int incoming_edges[], int V)
+{
+    int len = V;
+    int i;
+    for (i = 0; i < len; i++) {
+        if (incoming_edges[i] == 0) {
+            incoming_edges[i] = -1;
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 int main(int argc, char const *argv[])
 {
     int V;
@@ -37,33 +77,49 @@ int main(int argc, char const *argv[])
         }
     }
     
-    // haveCycle(graph[0], V);
-
-    int visited[V];
-    int row, flag = 0;
-    int index = -1;
-    int par = -1;
-
-    for (i = 0; i < V; i++) {
-        visited[i] = 0;
+    if(haveCycle(graph[0], V)) {
+        cout << "Had Loop" << endl;
+    } else {
+        cout << "Had no Loop" << endl;
     }
 
-    for (row = 0; row < V; row++) {   
-        
-        if (visited[row] == 0) {
-            flag = dfs(graph[0], row, V, visited, par);
-             
-            if (flag == 1) {
-                break;
-            }
+    int incoming_edges[V];
+    
+    for (i = 0; i < V; i++) {
+        incoming_edges[i] = 0;
+    }
 
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
+            if (graph[i][j] == 1) {
+                incoming_edges[j] += 1;            
+            }
         }
     }
 
-    if (flag == 1) {
-            cout << "Found Loop" << endl;
-    } else {
-        cout << "Loop Not Found" << endl;
+    cout << "Incomming edges : "; 
+    for (i = 0; i < V; i++) {
+        cout << incoming_edges[i] << " ";
+    }
+    cout << endl;
+
+    int count = V;
+    int tmp;
+    while (count) {
+        tmp = findZeroDeg(incoming_edges, V);
+
+        if (tmp == -1) {
+            cout << "Topological sort not possible" << endl;
+            break;
+        } else {
+            for (i = 0; i < V; i++) {
+                if (graph[tmp][i] == 1) {
+                    incoming_edges[i] -= 1;
+                }
+            }
+            cout << tmp + 1 << " ";
+        }
+        count--;
     }
 
     return 0;
